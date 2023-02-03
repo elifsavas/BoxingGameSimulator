5fb914071dfa9a42c7932c448213c9505e6a4ce1
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2023-02-03 15:30:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Fighter f1 = new Fighter("Cookie" , 10 , 120,105,30 ) ;
+        Fighter f2 = new Fighter("Alex", 20,85,103, 40) ;
+
+        Match match = new Match(f1,f2, 90,110) ;
+        match.run();
+
     }
 }
